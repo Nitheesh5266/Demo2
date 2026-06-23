d462cca06b991ad3ac63efbcb138a8ba48bea3dd
Nitheesh5266 <137703966+Nitheesh5266@users.noreply.github.com>
2026-06-23 17:43:21 +0530

    Update asdfghj.java
    deekshith

--- src/main/java/ApiTesting/asdfghj.java
+++ src/main/java/ApiTesting/asdfghj.java
@@ -1,7 +1,7 @@
 package ApiTesting;
 
-public class asdfghj {
+public class sai {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("hello Deekshith");
 	}
 }
